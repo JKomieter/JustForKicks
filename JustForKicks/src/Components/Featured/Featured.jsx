dba2f356b2c42df11ac1c095363c82afd787b90e
JKomieter <121212150+JKomieter@users.noreply.github.com>
2023-04-25 23:16:50 -0400

    tested the NavBar component

--- JustForKicks/src/Components/Featured/Featured.jsx
+++ JustForKicks/src/Components/Featured/Featured.jsx
@@ -9,6 +9,9 @@ function Featured() {
             <div className="topText">Week Featured Products</div>
             <div className="line"><hr/></div>
         </div>
+        <div className="words">
+            Typi non habent claritatem insitam est usus legentis in qui facit eorum claritatem, investigationes demonstraverunt lectores legere me lius quod legunt saepius.
+        </div>
     </div>
   )
 }
---
+++ JustForKicks/src/NavBar.test.jsx
@@ -0,0 +1,22 @@
+import { describe, expect, it } from "vitest";
+import { screen, render, fireEvent } from "@testing-library/react";
+import NavBar from "./Components/NavBar/NavBar";
+
+
+describe("NavBar Component", () => {
+    it("should render the NavBar comp directly", () => {
+        render(<NavBar/>)
+        const element = screen.findByText("Search");
+        expect(element).toBeDefined()
+    })
+    it("should show searchlist when user types in input", () => {
+        render(<NavBar/>);
+        const inputEl = screen.getByRole('textbox');
+        fireEvent.focus(inputEl);
+        const searchList = document.querySelector(".searchShoes")
+        expect(searchList).toBeDefined()
+
+
+    })
+
+})
